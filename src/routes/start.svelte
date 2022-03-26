a21291d7d8eacf94292f06234f0a6631399c80c3
trollar <glitchyfiles@gmail.com>
2022-03-26 18:25:20 +0000

    feat: :sparkles: engines can be specified from query

--- src/routes/start.svelte
+++ src/routes/start.svelte
@@ -1,37 +1,53 @@
-<script context="module">
-  export const prerender = true;
-</script>
 <script>
-  const engines = {
-    Bing: 'https://bing.com/search',
-    You: 'https://you.com/search',
-    Google: 'https://google.com/search',
-    DuckDuckGo: 'https://duckduckgo.com',
-    Qwant: 'https://qwant.com',
-    Yahoo: 'https://search.yahoo.com/search',
-    Linux: 'https://searx.projectlounge.pw/search'
-  }
-  let engine = engines.Bing
-	import { TextBox, ComboBox } from "fluent-svelte"
+	const engines = {
+		Bing: "https://bing.com/search",
+		You: "https://you.com/search",
+		Google: "https://google.com/search",
+		DuckDuckGo: "https://duckduckgo.com",
+		Qwant: "https://qwant.com",
+		Yahoo: "https://search.yahoo.com/search",
+		Linux: "https://searx.projectlounge.pw",
+		OpenCorporates: "https://opencorporates.com/search",
+	}
+	import { page } from "$app/stores"
+	import { TextBox, ComboBox, RadioButton } from "fluent-svelte"
+	import { fly } from "svelte/transition"
+	let engine = engines[$page.url.searchParams.get("engine")] ?? engines.Bing
 </script>
 
 <h1>trollarweb</h1>
 
 <form action={engine} target="_blank">
-  <TextBox name="q" type=search/>
-  <input type="hidden" name="t" value="h_" />
-  <input type="hidden" name="ia" value="web" />
+	<TextBox name="q" type="search" />
+	<input type="hidden" name="t" value="h_" />
+	<input type="hidden" name="ia" value="web" />
+	{#if engine === engines.OpenCorporates}
+		<nav in:fly={{ x: -10 }} out:fly={{ x: 10 }}>
+			<RadioButton name="type" value="companies">Companies</RadioButton>
+			<RadioButton name="type" value="officers">Officers</RadioButton>
+		</nav>
+	{/if}
 </form>
 
-<ComboBox items={Object.entries(engines).map(([name,value]) => ({ name,value }))} bind:value={engine} />
+<ComboBox
+	items={Object.entries(engines).map(([name, value]) => ({ name, value }))}
+	bind:value={engine} />
 
 <svelte:head>
-  <title>trollarweb</title>
-  <meta name="description" content="trolls your start page">
+	<title>trollarweb</title>
+	<meta name="description" content="trolls your start page" />
 </svelte:head>
 
 <style>
-  form {
-    margin-bottom: 2em;
-  }
+	form {
+		margin-bottom: 2em;
+	}
+	nav {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		gap: 1ch;
+		margin-top: 2em;
+		flex-direction: row;
+	}
 </style>
