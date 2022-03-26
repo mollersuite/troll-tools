222165ea3f575a45d6a936b3389f3067a44286a4
trollar <glitchyfiles@gmail.com>
2022-03-26 17:18:34 +0000

    chore: :tada: init commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "$lib/app.css"
+</script>
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      padding: 1rem;
+      width: 100%;
+      max-width: 1200px;
+      margin: 0 auto;
+      box-sizing: border-box;
+  }
+</style>
