2afad6ac1d62b67f42555aa9a0e6143016c0108e
trollar <glitchyfiles@gmail.com>
2022-03-26 17:50:30 +0000

    feat: :children_crossing: add landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,73 @@
 <script context="module">
-  export const prerender = true;
+	export const prerender = true
 </script>
+
 <script>
-  const engines = {
-    Bing: 'https://bing.com/search',
-    You: 'https://you.com/search',
-    Google: 'https://google.com/search',
-    DuckDuckGo: 'https://duckduckgo.com',
-    Qwant: 'https://qwant.com',
-    Yahoo: 'https://search.yahoo.com/search',
-    Linux: 'https://searx.projectlounge.pw/search'
-  }
-  let engine = engines.Bing
-	import { TextBox, ComboBox } from "fluent-svelte"
+	import GlobalSearch from "@fluentui/svg-icons/icons/globe_search_20_filled.svg?raw"
 </script>
 
-<h1>trollarweb</h1>
-
-<form action={engine} target="_blank">
-  <TextBox name="q" type=search/>
-  <input type="hidden" name="t" value="h_" />
-  <input type="hidden" name="ia" value="web" />
-  
-</form>
-
-<ComboBox items={Object.entries(engines).map(([name,value]) => ({ name,value }))} bind:value={engine} />
+<h1>troll</h1>
 
 <svelte:head>
-  <title>trollarweb</title>
-  <meta name="description" content="trolls your start page">
+	<title>troll</title>
+	<meta name="description" content="trolls your start page" />
 </svelte:head>
 
+<section>
+	<a sveltekit:prefetch href="/start">
+		<h1>{@html GlobalSearch} trollweb</h1>
+		<p>troll your world!</p>
+	</a>
+</section>
+
 <style>
-  form {
-    margin-bottom: 2em;
+  section :global(svg) {
+    fill: currentColor;
   }
+	section {
+		fill: currentColor;
+	}
+	section :global(h1) {
+		font-size: 1em;
+    display: flex;
+    align-items: center;
+    gap: 1ch;
+    flex-direction: row;
+	}
+	section {
+		column-count: auto;
+		column-width: 250px;
+		column-gap: 1em;
+	}
+	section :global(p) {
+		white-space: pre-wrap;
+	}
+	section :global(a) {
+		box-sizing: border-box;
+		max-height: 300px;
+		overflow-y: auto;
+		display: inline-block;
+		word-wrap: break-word;
+		width: 100%;
+		margin: 0 0 1em;
+		padding: 1rem;
+		background: var(--fds-solid-background-secondary);
+		border-radius: 1em;
+		color: var(--fds-text-primary);
+	}
+
+	section :global(a):hover {
+		text-decoration: none;
+	}
+	@supports (grid-template-rows: masonry) {
+		section {
+			display: grid;
+			column-gap: initial;
+			column-count: initial;
+			column-width: initial;
+			gap: 0 1em;
+			grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+			grid-template-rows: masonry;
+		}
+	}
 </style>
