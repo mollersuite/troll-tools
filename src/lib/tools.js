33ba019e7401b9f6cca2d5a6c7e6d5e688c16b14
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-27 18:28:09 -0400

    fix: :ambulance: fix link resolving

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,14 +1,14 @@
-export default Promise.all(Object.entries(import.meta.glob("../routes/tools/*.svelte")).map(
-	async ([href, load]) => {
+export default Promise.all(
+	Object.entries(import.meta.glob("../routes/tools/*.svelte")).map(async ([href, load]) => {
 		const { load: kitLoad } = await load()
 		const { stuff } = await kitLoad({
 			props: {},
 		})
 		return {
-			href: href.replace(".svelte", ""),
+			href: href.replace(".svelte", "").replace("/routes", ""),
 			name: stuff.name,
 			description: stuff.description,
 			icon: stuff.icon,
 		}
-	}
-))
+	})
+)
