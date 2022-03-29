aa5c320444328e464754cc887c7a11bd6a428e7a
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-28 22:26:22 -0400

    feat: :sparkles: Unicode font generator tool!

---
+++ src/routes/tools/font.svelte
@@ -0,0 +1,66 @@
+<script context="module">
+	import Font from "@fluentui/svg-icons/icons/text_font_20_filled.svg?raw"
+	import { TextBox } from "fluent-svelte"
+	export const prerender = true
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load() {
+		return {
+			stuff: {
+				name: 'Unicode "Font" Generator',
+				description: "make your tweets look cool at the expense of screen reader compatibility",
+				icon: Font,
+			},
+		}
+	}
+</script>
+
+<script>
+	import { InfoBar, Slider } from "fluent-svelte"
+
+	let value = ""
+	const zalgo = {
+		up: [
+			768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786,
+			787, 788, 789, 794, 795, 829, 830, 831, 832, 833, 834, 835, 836, 838, 842, 843, 844, 848, 849,
+			850, 855, 856, 859, 861, 862, 864, 865, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877,
+			878, 879,
+		],
+		middle: [820, 821, 822, 823, 824],
+		down: [
+			790, 791, 792, 793, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806, 807, 808, 809, 810,
+			811, 812, 813, 814, 815, 816, 817, 818, 819, 825, 826, 827, 828, 837, 839, 840, 841, 845, 846,
+			851, 852, 853, 854, 857, 858, 860, 863, 866,
+		],
+	}
+	/** @type {<Type>(arr: Type[]) => Type} */
+	const random = arr => arr[Math.floor(Math.random() * arr.length)]
+
+	const zalgo_chars = [...zalgo.up, ...zalgo.middle, ...zalgo.down]
+	let zalgo_intensity = 1
+</script>
+
+<TextBox autofocus bind:value />
+{#if value}
+	<h2>Zalgo</h2>
+	<Slider bind:value={zalgo_intensity} min={1} max={10} suffix=" characters per character" />
+	{[...value]
+		.map(
+			char =>
+				char +
+				Array.from({ length: zalgo_intensity }, () =>
+					String.fromCharCode(random(zalgo_chars))
+				).join("")
+		)
+		.join("")}
+	<h2>Underline</h2>
+	{[...value].map(char => char + "\u0332").join("")}
+	<h2>Strikethrough</h2>
+	{[...value].map(char => char + "\u0336").join("")}
+{:else}
+	<InfoBar
+		closable={false}
+		title="Type some text!"
+		message="We can convert your text to Zalgo, Underline, and Strikethrough." />
+{/if}
