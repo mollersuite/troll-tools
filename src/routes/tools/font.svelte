1037326f1fd527094b41dccfcaba979c4958f25b
Jack <29169102+Jack5079@users.noreply.github.com>
2022-03-29 12:41:00 -0400

    feat: :sparkles: Up, middle, and down settings for Zalgo in Unicode "Font" generator

--- src/routes/tools/font.svelte
+++ src/routes/tools/font.svelte
@@ -18,7 +18,8 @@
 
 <script>
 	import { browser } from "$app/env"
-	import { Button, Expander, InfoBar, Slider } from "fluent-svelte"
+	import { Button, Expander, InfoBar, Slider, ToggleSwitch } from "fluent-svelte"
+	import zalgo from "$lib/zalgo"
 
 	/** @param {string} text @param {HTMLElement} [paragraph] */
 	function say(text, paragraph) {
@@ -49,25 +50,19 @@
 	}
 
 	let value = ""
-	const zalgo = {
-		up: [
-			768, 769, 770, 771, 772, 773, 774, 775, 776, 777, 778, 779, 780, 781, 782, 783, 784, 785, 786,
-			787, 788, 789, 794, 795, 829, 830, 831, 832, 833, 834, 835, 836, 838, 842, 843, 844, 848, 849,
-			850, 855, 856, 859, 861, 862, 864, 865, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877,
-			878, 879,
-		],
-		middle: [820, 821, 822, 823, 824],
-		down: [
-			790, 791, 792, 793, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806, 807, 808, 809, 810,
-			811, 812, 813, 814, 815, 816, 817, 818, 819, 825, 826, 827, 828, 837, 839, 840, 841, 845, 846,
-			851, 852, 853, 854, 857, 858, 860, 863, 866,
-		],
-	}
 	/** @type {<Type>(arr: Type[]) => Type} */
 	const random = arr => arr[Math.floor(Math.random() * arr.length)]
 
-	const zalgo_chars = [...zalgo.up, ...zalgo.middle, ...zalgo.down]
+	let zalgo_up = true
+	let zalgo_middle = true
+	let zalgo_down = true
 	let zalgo_intensity = 1
+	$: zalgo_value = zalgo(value, {
+		up: zalgo_up,
+		middle: zalgo_middle,
+		down: zalgo_down,
+		intensity: zalgo_intensity,
+	})
 </script>
 
 {#if globalThis.speechSynthesis}
@@ -101,16 +96,13 @@
 {/if}
 {#if value}
 	<h2>Zalgo</h2>
-	<Slider bind:value={zalgo_intensity} min={1} max={10} suffix=" characters per character" />
-	{[...value]
-		.map(
-			char =>
-				char +
-				Array.from({ length: zalgo_intensity }, () =>
-					String.fromCharCode(random(zalgo_chars))
-				).join("")
-		)
-		.join("")}
+	<nav>
+		<Slider bind:value={zalgo_intensity} min={1} max={10} suffix=" characters per character" />
+		<ToggleSwitch bind:checked={zalgo_up}>Up</ToggleSwitch>
+		<ToggleSwitch bind:checked={zalgo_middle}>Middle</ToggleSwitch>
+		<ToggleSwitch bind:checked={zalgo_down}>Down</ToggleSwitch>
+	</nav>
+	{zalgo_value}
 	<h2>Underline</h2>
 	{[...value].map(char => char + "\u0332").join("")}
 	<h2>Strikethrough</h2>
