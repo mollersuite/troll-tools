6bed789fb7286830af26211d6dc213e9f92f8a75
Jack <29169102+Jack5079@users.noreply.github.com>
2022-04-30 00:25:16 -0400

    feat: :sparkles: codepoint viewer
    i wanna make it display the name but i cant seem to find a doable way to parse unicode data

---
+++ src/routes/tools/codepoints.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+	import Tool from "@fluentui/svg-icons/icons/scan_text_20_filled.svg?raw"
+	import { ListItem } from "fluent-svelte"
+	export const prerender = true
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load() {
+		return {
+			stuff: {
+				name: "Codepoints",
+				description: "See the Unicode codepoints inside text",
+				icon: Tool,
+			},
+		}
+	}
+</script>
+
+<script>
+	let value = ""
+</script>
+
+<textarea bind:value />
+{#each Array.from(value) as codepoint}
+	{@const fancy = codepoint.codePointAt().toString(16).toUpperCase().padStart(4, "0")}
+	<ListItem on:click={()=>navigator.clipboard.writeText(codepoint)}>U+{fancy} = <code>{codepoint}</code></ListItem>
+{/each}
